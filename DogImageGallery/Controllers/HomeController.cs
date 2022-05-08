133b806ecae1b79b73a27d9b5e6d339969c4ca69
Xinyu Wu <wuxinyu98@hotmail.com>
2022-05-08 01:43:06 -0400

    modify home controller

--- DogImageGallery/Controllers/HomeController.cs
+++ DogImageGallery/Controllers/HomeController.cs
@@ -1,26 +1,33 @@
-﻿using DogImageGallery.Models;
+﻿using DagImageGallery.Data;
+using DogImageGallery.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DogImageGallery.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IImage _imageService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IImage imageService)
         {
             _logger = logger;
+            _imageService = imageService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // return View();
+            var imageList = _imageService.Range(0, 12);
+
+            var model = new GalleryIndexModel()
+            {
+                Images = imageList
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
